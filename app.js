62143213112cef013449fb83b60c96a7ae3d0c9a
jimanjang <67411280+jimanjang@users.noreply.github.com>
2021-02-21 15:11:42 +0900

    Music Player

--- app.js
+++ app.js
@@ -1,22 +1,17 @@
-var inboxCheckboxes = document.querySelectorAll(".inbox-checkbox")
-var inboxStars = document.querySelectorAll(".inbox-star")
+var musicPlayItems = document.querySelectorAll(".music-play-item")
 
-inboxCheckboxes.forEach(function(item) {
-  item.addEventListener("click", toggle)
-  item.active = false
+musicPlayItems.forEach(function(item) {
+  item.isPlaying = false
+  item.audio = item.querySelector("audio")
+
+  item.addEventListener("click", function() {
+    if (this.isPlaying) {
+      item.audio.pause()
+      item.audio.currentTime = 0
+    } else {
+      item.audio.play()
+    }
+
+    item.isPlaying = !item.isPlaying
+  })
 })
-
-inboxStars.forEach(function(item) {
-  item.addEventListener("click", toggle)
-  item.active = false
-})
-
-function toggle() {
-  if (this.active) {
-    this.classList.remove("active")
-  } else {
-    this.classList.add("active")
-  }
-
-  this.active = !this.active
-}
