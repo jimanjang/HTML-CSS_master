6355afa7761fe03bb114d04ca761d243e837845d
jimanjang <67411280+jimanjang@users.noreply.github.com>
2021-02-21 11:28:13 +0900

    feed

--- app.js
+++ app.js
@@ -1,14 +1,38 @@
-var dropdownMenu = document.querySelector(".dropdown-menu")
-var dropdownButton = document.querySelector(".dropdown-button")
+var followButton = document.querySelector(".feed-user-profile button")
+var likeButton = document.querySelector(".feed-footer button:first-child")
+var commentButton = document.querySelector(".feed-footer button:last-child")
+var feedComment = document.querySelector(".feed-comment")
 
-dropdownButton.addEventListener("click", function(event) {
-  if (this.active) {
-    dropdownMenu.classList.remove("active")
+followButton.addEventListener("click", function() {
+  if (this.following) {
+    this.innerHTML = "Follow"
+    this.classList.remove("following")
   } else {
-    dropdownMenu.classList.add("active")
+    this.innerHTML = "Following"
+    this.classList.add("following")
   }
+  this.following = !this.following
+})
+followButton.following = false
 
+likeButton.addEventListener("click", function() {
+  if (this.following) {
+    this.innerHTML = "10 Likes"
+    this.classList.remove("active")
+  } else {
+    this.innerHTML = "11 Likes"
+    this.classList.add("active")
+  }
+  this.following = !this.following
+})
+likeButton.following = false
+
+commentButton.addEventListener("click", function() {
+  if (this.active) {
+    feedComment.classList.remove("active")
+  } else {
+    feedComment.classList.add("active")
+  }
   this.active = !this.active
 })
-
-dropdownButton.active = false
+commentButton.active = false
